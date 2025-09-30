24c8d8fd9f5b921f588c180dec934042566c9e37
AgusPW <agustinbaena@hotmail.com>
2025-10-01 01:26:39 +0200

    chore: WIP antes de rebase

--- consumer/config.py
+++ consumer/config.py
@@ -3,7 +3,7 @@ import os
 
 # Variables de entorno para RabbitMQ (opcional, pero recomendable)
 RABBITMQ_USER = os.getenv("RABBITMQ_USER", "userapp_rabbitmq")
-RABBITMQ_PASSWORD = os.getenv("RABBITMQ_PASSWORD", "dcji484hf8ft4")
+RABBITMQ_PASSWORD = os.getenv("RABBITMQ_PASSWORD", "xxxx")
 RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", "localhost")
 
 ENDPOINT_URL = os.getenv("ENDPOINT_URL", "http://127.0.0.1:1234")
